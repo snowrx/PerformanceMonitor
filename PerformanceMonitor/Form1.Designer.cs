857ad63d16e3ee906ad96109c7f88d51efa7883e
すのー* <sal.snow3510+github@gmail.com>
2020-02-15 00:44:39 +0900

    Initial commit

---
+++ PerformanceMonitor/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PerformanceMonitor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(149, 42);
+			// 
+			// closeToolStripMenuItem
+			// 
+			this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+			this.closeToolStripMenuItem.Size = new System.Drawing.Size(148, 38);
+			this.closeToolStripMenuItem.Text = "Close";
+			this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(232, 200);
+			this.DoubleBuffered = true;
+			this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.Color.White;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Performance Monitor";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+	}
+}
+
---
+++ PerformanceMonitor/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+
+namespace PerformanceMonitor
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x00000020;
+				return cp;
+			}
+		}
+
+		private readonly int GraphHeight = 10;
+		private readonly int GraphWidth = 200;
+		private readonly int GraphMargin = 1;
+		private PerformanceCounter ProcessorTime = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+		private PerformanceCounter ProcessorPerformance = new PerformanceCounter("Processor Information", "% Processor Performance", "_Total");
+		private ManagementClass ManagementClass = new ManagementClass("Win32_OperatingSystem");
+		private PictureBox ProcessorTimeGraph = new PictureBox();
+		private PictureBox ProcessorPerformanceLow = new PictureBox();
+		private PictureBox ProcessorPerformanceHigh = new PictureBox();
+		private PictureBox MemoryGraph = new PictureBox();
+		private int ProcessorTimeGraphWidth = 0;
+		private int ProcessorPerformanceLowWidth = 0;
+		private int ProcessorPerformanceHighWidth = 0;
+		private int MemoryGraphWidth = 0;
+		private int interval = 250;
+		private bool Working = true;
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				SuspendLayout();
+				ShowInTaskbar = false;
+				TopMost = true;
+				Height = (GraphHeight * 3) + (GraphMargin * 2);
+				Width = GraphWidth;
+				Top = 0;
+				Left = Screen.PrimaryScreen.Bounds.Width - Width;
+				TransparencyKey = BackColor;
+				Opacity = 0.6;
+				notifyIcon1.Text = Text;
+
+				ProcessorTimeGraph.Left = 0;
+				ProcessorTimeGraph.Top = 0;
+				ProcessorTimeGraph.Width = GraphWidth;
+				ProcessorTimeGraph.Height = GraphHeight;
+				ProcessorTimeGraph.BackColor = Color.LawnGreen;
+				Controls.Add(ProcessorTimeGraph);
+
+				ProcessorPerformanceHigh.Left = 0;
+				ProcessorPerformanceHigh.Top = (GraphHeight * 1) + (GraphMargin * 1);
+				ProcessorPerformanceHigh.Width = GraphWidth;
+				ProcessorPerformanceHigh.Height = GraphHeight;
+				ProcessorPerformanceHigh.BackColor = Color.OrangeRed;
+				Controls.Add(ProcessorPerformanceHigh);
+
+				ProcessorPerformanceLow.Location = ProcessorPerformanceHigh.Location;
+				ProcessorPerformanceLow.Size = ProcessorPerformanceHigh.Size;
+				ProcessorPerformanceLow.BackColor = Color.CornflowerBlue;
+				Controls.Add(ProcessorPerformanceLow);
+
+				MemoryGraph.Left = 0;
+				MemoryGraph.Top = (GraphHeight * 2) + (GraphMargin * 2);
+				MemoryGraph.Width = GraphWidth;
+				MemoryGraph.Height = GraphHeight;
+				MemoryGraph.BackColor = Color.Violet;
+				Controls.Add(MemoryGraph);
+				ResumeLayout();
+
+				Task.Run(MonitorWorker);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+				Close();
+			}
+		}
+
+		private void MonitorWorker()
+		{
+			DateTime laststart = DateTime.Now;
+			while (Working)
+			{
+				DateTime start = DateTime.Now;
+				//Debug.WriteLine((start - laststart).TotalMilliseconds);
+				var pp = ProcessorPerformance.NextValue();
+				var pt = ProcessorTime.NextValue();
+				var moc = ManagementClass.GetInstances();
+				var mem = 0f;
+				foreach (var mo in moc)
+				{
+					var total = float.Parse($"{mo["TotalVisibleMemorySize"]}");
+					var free = float.Parse($"{mo["FreePhysicalMemory"]}");
+					mem = (total - free) * 100 / total;
+				}
+				ProcessorPerformanceLowWidth = (int)Math.Min(GraphWidth, Math.Round(pp * GraphWidth / 100));
+				ProcessorPerformanceHighWidth = (int)Math.Max(0, Math.Round((pp - 100) * GraphWidth / 100));
+				ProcessorTimeGraphWidth = (int)Math.Round(pt * GraphWidth / 100);
+				MemoryGraphWidth = (int)Math.Round(mem * GraphWidth / 100);
+
+				Invoke((FormUpdate)GraphUpdate);
+				DateTime finish = DateTime.Now;
+				var delay = Math.Max(0, interval - (int)Math.Round((finish - start).TotalMilliseconds));
+				Debug.WriteLine(delay);
+				Thread.Sleep(delay);
+				laststart = start;
+			}
+		}
+
+		private delegate void FormUpdate();
+		private void GraphUpdate()
+		{
+			ProcessorTimeGraph.Width = ProcessorTimeGraphWidth;
+			ProcessorPerformanceLow.Width = ProcessorPerformanceLowWidth;
+			ProcessorPerformanceHigh.Width = ProcessorPerformanceHighWidth;
+			MemoryGraph.Width = MemoryGraphWidth;
+		}
+
+		private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Working = false;
+			Thread.Sleep(interval);
+			Close();
+		}
+	}
+}
