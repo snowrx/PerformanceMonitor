b4af019017fecfd86c73b938d7e807fe58b3f708
すのー* <sal.snow3510+github@gmail.com>
2020-02-15 20:46:37 +0900

    cleanup

--- PerformanceMonitor/Form1.cs
+++ PerformanceMonitor/Form1.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Management;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
-using System.Management;
 
 namespace PerformanceMonitor
 {
@@ -33,35 +28,40 @@ namespace PerformanceMonitor
 		private readonly int GraphHeight = 10;
 		private readonly int GraphWidth = 200;
 		private readonly int GraphMargin = 1;
-		private PerformanceCounter ProcessorTime = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
-		private PerformanceCounter ProcessorPerformance = new PerformanceCounter("Processor Information", "% Processor Performance", "_Total");
-		private PerformanceCounter IdleTime = new PerformanceCounter("Processor Information", "% Idle Time", "_Total");
-		private ManagementClass ManagementClass = new ManagementClass("Win32_OperatingSystem");
-		private PictureBox ProcessorTimeGraph = new PictureBox();
-		private PictureBox ProcessorPerformanceLow = new PictureBox();
-		private PictureBox ProcessorPerformanceHigh = new PictureBox();
-		private PictureBox MemoryGraph = new PictureBox();
+		private readonly PerformanceCounter ProcessorTime = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+		private readonly PerformanceCounter ProcessorPerformance = new PerformanceCounter("Processor Information", "% Processor Performance", "_Total");
+		private readonly PerformanceCounter IdleTime = new PerformanceCounter("Processor Information", "% Idle Time", "_Total");
+		private readonly ManagementClass ManagementClass = new ManagementClass("Win32_OperatingSystem");
+		private readonly PictureBox ProcessorTimeGraph = new PictureBox();
+		private readonly PictureBox ProcessorPerformanceLow = new PictureBox();
+		private readonly PictureBox ProcessorPerformanceHigh = new PictureBox();
+		private readonly PictureBox MemoryGraph = new PictureBox();
 		private int ProcessorTimeGraphWidth = 0;
 		private int ProcessorPerformanceLowWidth = 0;
 		private int ProcessorPerformanceHighWidth = 0;
 		private int MemoryGraphWidth = 0;
-		private int interval = 250;
+		private readonly int interval = 250;
 		private bool Working = true;
+
+		private void ResetForm()
+		{
+			ShowInTaskbar = false;
+			TopMost = true;
+			Height = (GraphHeight * 3) + (GraphMargin * 2);
+			Width = GraphWidth;
+			Top = 0;
+			Left = Screen.PrimaryScreen.Bounds.Width - Width;
+			TransparencyKey = BackColor;
+			Opacity = 0.6;
+			notifyIcon1.Text = Text;
+		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			try
 			{
 				SuspendLayout();
-				ShowInTaskbar = false;
-				TopMost = true;
-				Height = (GraphHeight * 3) + (GraphMargin * 2);
-				Width = GraphWidth;
-				Top = 0;
-				Left = Screen.PrimaryScreen.Bounds.Width - Width;
-				TransparencyKey = BackColor;
-				Opacity = 0.6;
-				notifyIcon1.Text = Text;
+				ResetForm();
 
 				ProcessorTimeGraph.Left = 0;
 				ProcessorTimeGraph.Top = 0;
@@ -90,6 +90,7 @@ namespace PerformanceMonitor
 				Controls.Add(MemoryGraph);
 				ResumeLayout();
 
+				GC.Collect();
 				Task.Run(MonitorWorker);
 			}
 			catch (Exception ex)
@@ -102,18 +103,27 @@ namespace PerformanceMonitor
 		private void MonitorWorker()
 		{
 			DateTime laststart = DateTime.Now;
+			DateTime start = DateTime.Now;
+			float pp = ProcessorPerformance.NextValue() * (100 - IdleTime.NextValue()) / 100;
+			float pt = ProcessorTime.NextValue();
+			ManagementObjectCollection moc = ManagementClass.GetInstances();
+			float mem = 0f;
+			float total = 0f;
+			float free = 0f;
+
 			while (Working)
 			{
-				DateTime start = DateTime.Now;
-				//Debug.WriteLine((start - laststart).TotalMilliseconds);
-				var pp = ProcessorPerformance.NextValue() * (100 - IdleTime.NextValue()) / 100;
-				var pt = ProcessorTime.NextValue();
-				var moc = ManagementClass.GetInstances();
-				var mem = 0f;
-				foreach (var mo in moc)
+				laststart = start;
+				start = DateTime.Now;
+				Debug.WriteLine(Math.Round((start - laststart).TotalMilliseconds));
+				pp = ProcessorPerformance.NextValue() * (100 - IdleTime.NextValue()) / 100;
+				pt = ProcessorTime.NextValue();
+				moc = ManagementClass.GetInstances();
+				mem = 0f;
+				foreach (ManagementBaseObject mo in moc)
 				{
-					var total = float.Parse($"{mo["TotalVisibleMemorySize"]}");
-					var free = float.Parse($"{mo["FreePhysicalMemory"]}");
+					total = float.Parse($"{mo["TotalVisibleMemorySize"]}");
+					free = float.Parse($"{mo["FreePhysicalMemory"]}");
 					mem = (total - free) * 100 / total;
 				}
 				ProcessorPerformanceLowWidth = (int)Math.Min(GraphWidth, Math.Round(pp * GraphWidth / 100));
@@ -123,10 +133,7 @@ namespace PerformanceMonitor
 
 				Invoke((FormUpdate)GraphUpdate);
 				DateTime finish = DateTime.Now;
-				var delay = Math.Max(0, interval - (int)Math.Round((finish - start).TotalMilliseconds));
-				Debug.WriteLine(delay);
-				Thread.Sleep(delay);
-				laststart = start;
+				Thread.Sleep(Math.Max(0, interval - (int)Math.Round((finish - start).TotalMilliseconds)));
 			}
 		}
 
@@ -144,6 +151,14 @@ namespace PerformanceMonitor
 			Working = false;
 			Thread.Sleep(interval);
 			Close();
+		}
+
+		private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				ResetForm();
+			}
 		}
 	}
 }
